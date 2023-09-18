bea025ab7f673bb19e3e38c7056a3068865c16b0
robertnjunge <mubearobert@gmail.com>
2023-09-18 17:03:44 +0300

    completed code challenge

--- lib/main.py
+++ lib/main.py
@@ -1,3 +1,12 @@
+
+# Question 1: 
+# Implement a function is_balanced(expression) that takes a string 
+# containing parentheses, curly braces, and square brackets,and determines whether 
+# the expression is balanced. 
+
+# An expression is considered balanced if each opening bracket has a corresponding closing 
+# bracket in the correct order.
+
 def is_balanced(expression):
     stack = []
     mapping = {')': '(', '}': '{', ']': '['}
@@ -18,14 +27,31 @@ print(is_balanced(expression1))  # Output: True
 print(is_balanced(expression2))  # Output: False
 
 
+# Question 2:
+# Write a function remove_duplicates(sequence) that takes a 
+# sequence (list or tuple) and returns a new sequence with duplicates 
+# removed while maintaining the original order of elements. 
 
 def remove_duplicates(sequence):
-    return list(dict.fromkeys(sequence))
+    unique_elements = []
+    for item in sequence:
+        if item not in unique_elements:
+            unique_elements.append(item)
+    return unique_elements
+
 
 # Test case
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 result = remove_duplicates(input_sequence)
 print(result)  # Output: [2, 3, 4, 5, 6, 7]
+
+
+# Question 3: 
+# Implement a function word_frequency(sentence) that takes 
+# a sentence and returns a dictionary containing the frequency of each 
+# word in the sentence. 
+
+# Ignore punctuation and consider words in a case-insensitive manner.
 
 import string
 
@@ -33,8 +59,15 @@ def word_frequency(sentence):
     sentence = sentence.lower()
     sentence = sentence.translate(str.maketrans('', '', string.punctuation))
     words = sentence.split()
-    
-    return {word: words.count(word) for word in set(words)}
+
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
 
 # Test case
 sentence = "This is a test sentence. This sentence is a test."
